584f2f696c970514e801e163eb3c2fb6a2f7aa83
AnkitBisen <bisenankit21@gmail.com>
2024-05-17 12:35:18 +0530

    Initial commit

--- src/main/java/com/example/springboot/crudDemoMainJPARepo/JPARepository/EmployeeRepository.java
+++ src/main/java/com/example/springboot/crudDemoMainJPARepo/JPARepository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.example.springboot.crudDemoMainJPARepo.JPARepository;
 
 import com.example.springboot.crudDemoMainJPARepo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+//@RepositoryRestResource(path = "members")  using this annotation we can access all the endpoints usinf /localhost/magic-api/members
+ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
